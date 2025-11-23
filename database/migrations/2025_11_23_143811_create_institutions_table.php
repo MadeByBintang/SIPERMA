ea9230340016e639e9bd3218908f963ff4680121
Yoruuu00 <2310817310014@mhs.ulm.ac.id>
2025-11-23 22:43:31 +0800

    migrate buat tabel institutions

---
+++ database/migrations/2025_11_23_143811_create_institutions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    // Cek dulu biar gak error kalau tabelnya udah ada
+    if (!Schema::hasTable('institutions')) {
+        Schema::create('institutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('address')->nullable();
+
+            // Kolom tambahan yang tadi kita buat
+            $table->string('sector', 50);
+            $table->string('owner_name', 100);
+            $table->string('owner_email', 100)->nullable();
+            $table->string('owner_phone', 20)->nullable();
+
+            $table->timestamps();
+        });
+    }
+}
+
+public function down()
+{
+    Schema::dropIfExists('institutions');
+}
+};
