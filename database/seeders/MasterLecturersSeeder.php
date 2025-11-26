b0313b55c1c798f1080d0ce366b55d27ae2fba70
BINZ <adrianbintang3@gmail.com>
2025-11-26 11:38:24 +0700

    fix: update MasterLecturersSeeder and MasterStudentsSeeder with new lecturer and student data

--- database/seeders/MasterLecturersSeeder.php
+++ database/seeders/MasterLecturersSeeder.php
@@ -12,22 +12,79 @@ class MasterLecturersSeeder extends Seeder
      */
     public function run(): void
     {
-        MasterLecturer::create([
-            'nip' => '198503102010011001',
-            'full_name' => 'Dr. Budi Santoso, M.Kom.',
-            'email' => 'budi.santoso@ulm.ac.id'
-        ]);
+        $lecturers = [
+            [
+                'nip' => '197608052003121004',
+                'full_name' => 'JUHRIYANSYAH DALLE',
+                'email' => 'j.dalle@ulm.ac.id'
+            ],
+            [
+                'nip' => '198205082008011010',
+                'full_name' => 'EKA SETYA WIJAYA',
+                'email' => 'ekasw@ulm.ac.id'
+            ],
+            [
+                'nip' => '198411202015042002',
+                'full_name' => 'YUSLENA SARI',
+                'email' => 'yuzlena@ulm.ac.id'
+            ],
+            [
+                'nip' => '198606132015041001',
+                'full_name' => 'MUHAMMAD ALKAFF',
+                'email' => 'm.alkaff@ulm.ac.id'
+            ],
+            [
+                'nip' => '198810272019032013',
+                'full_name' => 'MUTI\'A MAULIDA',
+                'email' => 'mutia.maulida@ulm.ac.id'
+            ],
+            [
+                'nip' => '199307032019031011',
+                'full_name' => 'ANDREYAN RIZKY BASKARA',
+                'email' => 'andreyan.baskara@ulm.ac.id'
+            ],
+            [
+                'nip' => '199110252019032018',
+                'full_name' => 'NURUL FATHANAH MUSTAMIN',
+                'email' => 'nurul.mustamin@ulm.ac.id'
+            ],
+            [
+                'nip' => '199106192024062001',
+                'full_name' => 'HELDA YUNITA',
+                'email' => 'helda.yunita@ulm.ac.id'
+            ],
+            [
+                'nip' => '199710312025061009',
+                'full_name' => 'IRHAM MAULANI ABDUL GANI',
+                'email' => 'irhammag.199710312025061009@ulm.ac.id'
+            ],
+            [
+                'nip' => '199807102025061010',
+                'full_name' => 'ACHMAD MUJADDID ISLAMI',
+                'email' => 'achmadmi.199807102025061010@ulm.ac.id'
+            ],
+            [
+                'nip' => '200006192025062016',
+                'full_name' => 'ERIKA MAULIDIYA',
+                'email' => 'erikam.200006192025062016@ulm.ac.id'
+            ],
+            [
+                'nip' => '199611092023211009',
+                'full_name' => 'MUHAMMAD FAJRIAN NOOR',
+                'email' => 'fajrian.noor@ulm.ac.id'
+            ],
+            [
+                'nip' => '198904162024211002',
+                'full_name' => 'MUHAMMAD BAHIT',
+                'email' => 'muhammadbahit@ulm.ac.id'
+            ],
+            [
+                'nip' => '1981090420181110000',
+                'full_name' => 'FADLIYANUR',
+                'email' => 'fadliyanur@ulm.ac.id'
+            ]
+        ];
 
-        MasterLecturer::create([
-            'nip' => '199005202015032002',
-            'full_name' => 'Rina Wijaya, S.T., M.T.',
-            'email' => 'rina.wijaya@ulm.ac.id'
-        ]);
-
-        MasterLecturer::create([
-            'nip' => '200006192025062016',
-            'full_name' => 'Erika Maulidiya, S.Kom., M.Kom',
-            'email' => 'erikamaulidiya@ulm.ac.id'
-        ]);
+        MasterLecturer::insert($lecturers);
     }
 }
