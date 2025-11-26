d5179aa3dfacac7fd8b688ae944cbbe2759b6b81
BINZ <adrianbintang3@gmail.com>
2025-11-26 13:32:47 +0700

    refactor: streamline FocusSeeder to use focus list for lecturer and student updates

--- database/seeders/FocusSeeder.php
+++ database/seeders/FocusSeeder.php
@@ -2,38 +2,40 @@
 
 namespace Database\Seeders;
 
-use App\Models\Student;
-use App\Models\Lecturer;
+use Illuminate\Database\Seeder;
 use App\Models\MasterLecturer;
 use App\Models\MasterStudent;
-use Illuminate\Database\Seeder;
+use App\Models\Lecturer;
+use App\Models\Student;
 
 class FocusSeeder extends Seeder
 {
     public function run(): void
     {
-        $data_lecturer = [
-            '198503102010011001' => 'BIG DATA',
-            '199005202015032002' => 'MTI',
-            '200006192025062016' => 'JARINGAN',
-        ];
+        $focusList = ['BIG DATA', 'MTI', 'JARINGAN'];
 
-        $data_student = [
-            '2310817310009' => 'BIG DATA',
-            '2310817310012' => 'MTI',
-            '9910817119999' => 'JARINGAN',
-        ];
+        // update lecturer
+        MasterLecturer::all()->each(function ($master) use ($focusList) {
 
-        foreach ($data_student as $nim => $focus) {
-            $master = MasterStudent::where('nim', $nim)->first();
-            $student = Student::where('master_student_id', $master->master_student_id)->first();
-            $student->update(['focus' => $focus]);
-        }
-
-        foreach ($data_lecturer as $nip => $focus) {
-            $master = MasterLecturer::where('nip', $nip)->first();
             $lecturer = Lecturer::where('master_lecturer_id', $master->master_lecturer_id)->first();
-            $lecturer->update(['focus' => $focus]);
-        }
+
+            if ($lecturer) {
+                $lecturer->update([
+                    'focus' => $focusList[array_rand($focusList)]
+                ]);
+            }
+        });
+
+        // update student
+        MasterStudent::all()->each(function ($master) use ($focusList) {
+
+            $student = Student::where('master_student_id', $master->master_student_id)->first();
+
+            if ($student) {
+                $student->update([
+                    'focus' => $focusList[array_rand($focusList)]
+                ]);
+            }
+        });
     }
 }
